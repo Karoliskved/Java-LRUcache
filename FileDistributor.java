7a74ba7efee846dac2c9fbc0bf33cf787e36ba8e
Karoliskved <79286725+Karoliskved@users.noreply.github.com>
2024-06-17 08:30:01 +0300

    Add files via upload

--- FileDistributor.java
+++ FileDistributor.java
@@ -4,29 +4,37 @@ import java.io.File;
 
 public class FileDistributor
 {
-	static int dirCount = 500;
+	private int dirCount;
 
 	public static void main(String[] args)
 	{
-		String filepath = args[0];
-		File file = new File(filepath);
-		FileDistributor fileDistributor = new FileDistributor();
 
-		for (File individualFile : file.listFiles())
+		if (args.length == 0)
 		{
-			if (individualFile.isFile())
-				fileDistributor.recieveFile(individualFile, filepath);
+			manageDistribution(".", 500);
 		}
+		else
+		{
+			String filepath = args[0];
+			manageDistribution(filepath, 500);
+
+		}
+
 		// fileDistributor.findFile("testingFile1.txt", filepath);
 		// fileDistributor.findFile("testingFile2.txt", filepath);
 		// fileDistributor.findFile("testingFile3.txt", filepath);
+	}
+
+	public FileDistributor(int dirCount)
+	{
+		this.dirCount = dirCount;
 	}
 
 	public void recieveFile(File file, String filepath)
 	{
 		String dirName = Integer.toString(Math.abs(file.getName().hashCode() % dirCount));
 
-		File directory = new File(filepath + "/" + dirName);
+		File directory = new File(filepath + dirName);
 
 		if (directory.mkdir())
 		{
@@ -34,16 +42,15 @@ public class FileDistributor
 		}
 		else
 		{
-			System.out.println("Failed to create directory or it already exists.");
+			// System.out.println("Failed to create directory or it already exists.");
 		}
-		String testNAme = filepath + "/" + dirName + "/" + file.getName();
-		file.renameTo(new File(filepath + "/" + dirName + "/" + file.getName()));
+		file.renameTo(new File(filepath + dirName + "/" + file.getName()));
 
 	}
 
 	public File findFile(String fileName, String filepath)
 	{
-		String dirName = filepath + "/" + Integer.toString(Math.abs(fileName.hashCode() % dirCount));
+		String dirName = filepath + Integer.toString(Math.abs(fileName.hashCode() % dirCount));
 		File directory = new File(dirName);
 		for (File individualFile : directory.listFiles())
 		{
@@ -54,5 +61,22 @@ public class FileDistributor
 			}
 		}
 		return null;
+	}
+
+	private static void manageDistribution(String fileName, int dirCount)
+	{
+		FileDistributor fileDistributor = new FileDistributor(dirCount);
+		File file = new File(fileName);
+		System.out.println(file.getAbsolutePath());
+
+		File[] fileList = file.listFiles();
+		if (fileList != null)
+		{
+			for (File individualFile : fileList)
+			{
+				if (individualFile.isFile())
+					fileDistributor.recieveFile(individualFile, "");
+			}
+		}
 	}
 }
--- FileGenerator.java
+++ FileGenerator.java
@@ -2,19 +2,31 @@ package fileDistributor;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 
 public class FileGenerator
 {
-	static int dirCount = 500;
 
 	public static void main(String[] args)
 	{
-		String filepath = args[0];
+		if (args.length == 0)
+		{
+			generateTestFiles("");
+		}
+		else
+		{
+			String filepath = args[0];
+			generateTestFiles(filepath + "/");
+		}
+
+	}
+
+	public static void generateTestFiles(String filepath)
+	{
 		File file = new File(filepath);
-		FileDistributor fileDistributor = new FileDistributor();
 		for (int i = 0; i < 10000; i++)
 		{
-			File newFile = new File(filepath + "/testingFile" + i + ".txt");
+			File newFile = new File(filepath + "testingFile" + UUID.randomUUID().toString() + ".txt");
 			try
 			{
 				newFile.createNewFile();
@@ -25,7 +37,5 @@ public class FileGenerator
 				e.printStackTrace();
 			}
 		}
-
 	}
-
 }
